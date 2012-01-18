ad84ae59b7831b0649fded853d11954551d0878f
Martin <digitalmannen@gmail.com>
2012-01-18 17:17:48 +0100

    Added timestamps

--- src/se/digitalmannen/svTwUni/SvTwuni.java
+++ src/se/digitalmannen/svTwUni/SvTwuni.java
@@ -170,7 +170,15 @@ public class SvTwuni {
 			public void actionPerformed(ActionEvent arg0) {
 				getAllTextFields();
 				if (verfiyeTextField()){
-					writeTable();
+					//TODO make to threads
+					GetLectureTweets lt = new GetLectureTweets();
+					GetTagTweet tt = new GetTagTweet();
+					WriteTable wt = new WriteTable();
+					wt.writeTableToFile(lt,tt,tag,tagStartDate,
+							tagStartTime,tagEndDate,tagEndTime,
+							svTwUni,lectureStartDate,lectureStartTime,
+							lectureEndDate,lectureEndTime, lectureName);
+					//writeTable();
 				}
 			}
 		});
@@ -187,80 +195,82 @@ public class SvTwuni {
 		mainForm.getContentPane().add(writeProgressBar);
 	}
 
-	private void writeTable(){
-		GetLectureTweets lt = new GetLectureTweets();
-		GetTagTweet tt = new GetTagTweet();
-		//lTweets =  lt.getTweetArray("dmutv", "2011-11-01", "22:59:00", "2011-11-01", "23:01:00");
-		//tTweets = tt.getTweetArray("tsttwsv", "2011-11-01", "22:59:00", "2011-11-01", "23:01:00");
-		//FetchLectureProssesLabel.setText("Working...");
-		lTweets =  lt.getTweetArray(svTwUni,lectureStartDate,lectureStartTime,lectureEndDate,lectureEndTime);
-		//FetchLectureProssesLabel.setText("Done");
-		//FetchtagProsseslabel.setText("Working...");
-		tTweets = tt.getTweetArray(tag,tagStartDate,tagStartTime,tagEndDate,tagEndTime);
-		//FetchtagProsseslabel.setText("Done");
-		 Tweet[] ltArray = new Tweet[lTweets.size()];
-		 lTweets.toArray(ltArray);
-		 
-		 Tweet[] ttArray = new Tweet[tTweets.size()];
-		 tTweets.toArray(ttArray);
-		 
-		 //Progressbaeren
-		 writeProgressBar.setMaximum(ltArray.length-1);
-		 
-		 //start the writing
-		try {
-			// Create file 
-			  FileWriter fstream = new FileWriter(tag+".txt");
-			  BufferedWriter outFile = new BufferedWriter(fstream);
-			 
-			  outFile.write("<table border=1>\n");
-			 outFile.write("<tr><td><h3>Lecturer</h3><h4>"+ lectureName + "</h4></td><td><h3>Room</h3><h4>#"+ tag + "</h4></td></tr>\n");
-			 for (int i=0; i < ltArray.length;i++){
-				 outFile.write("<tr>\n");
-				 outFile.write("<td valign='top' width='55%'>" +URLInString.makeURLInString(ltArray[i].getText()) + "</td><td bgcolor='#E0E0E0' valign='top'> <td>\n</tr>\n");
-				 //F8F8F8
-				 //outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
-				 if (i < ltArray.length -1){
-					 for (int j =0; j < ttArray.length; j++){
-						 if (ttArray[j].getCreatedAt().after(ltArray[i].getCreatedAt()) 
-								 && ttArray[j].getCreatedAt().before(ltArray[i+1].getCreatedAt())){
-							 outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
-							 outFile.write("<a href='https://twitter.com/#!/"
-									 + ttArray[j].getFromUser() 
-									 +  "' target='_blank'>@" +ttArray[j].getFromUser() + "</a>\n");
-							 outFile.write(URLInString.makeURLInString(ttArray[j].getText()) + "</td></tr>\n"); 
-						 }	 
-					 }
-				 }
-				 else {
-					 for (int j =0; j < ttArray.length; j++){
-						 if (ttArray[j].getCreatedAt().after(ltArray[i].getCreatedAt())){
-							 outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
-							 outFile.write("<a href='https://twitter.com/#!/"
-									 + ttArray[j].getFromUser() 
-									 +  "' target='_blank'>@" +ttArray[j].getFromUser() + "</a>\n");
-							 outFile.write(URLInString.makeURLInString(ttArray[j].getText()) + "</td></tr>\n"); 
-						 }	 
-					 }
-					 
-				 }
-					 
-				 //outFile.write("</td>");
-				 //outFile.write("</tr>\n");
-				 writeProgressBar.setValue(i);
-				mainForm.repaint();
-				 
-				 
-			 }
-			 outFile.write("</table>\n");
-			//Close the output stream
-			 outFile.close();
-			 //System.out.println("Done");
-			 
-		} catch (IOException e) {
-			System.err.println("Error: " + e.getMessage());
-		}
-	}
+//	private void writeTable(){
+//		GetLectureTweets lt = new GetLectureTweets();
+//		GetTagTweet tt = new GetTagTweet();
+//		//lTweets =  lt.getTweetArray("dmutv", "2011-11-01", "22:59:00", "2011-11-01", "23:01:00");
+//		//tTweets = tt.getTweetArray("tsttwsv", "2011-11-01", "22:59:00", "2011-11-01", "23:01:00");
+//		//FetchLectureProssesLabel.setText("Working...");
+//		lTweets =  lt.getTweetArray(svTwUni,lectureStartDate,lectureStartTime,lectureEndDate,lectureEndTime);
+//		//FetchLectureProssesLabel.setText("Done");
+//		//FetchtagProsseslabel.setText("Working...");
+//		tTweets = tt.getTweetArray(tag,tagStartDate,tagStartTime,tagEndDate,tagEndTime);
+//		//FetchtagProsseslabel.setText("Done");
+//		 Tweet[] ltArray = new Tweet[lTweets.size()];
+//		 lTweets.toArray(ltArray);
+//		 
+//		 Tweet[] ttArray = new Tweet[tTweets.size()];
+//		 tTweets.toArray(ttArray);
+//		 
+//		 //Progressbaeren
+//		 writeProgressBar.setMaximum(ltArray.length-1);
+//		 writeProgressBar.setIndeterminate(true);
+//		 //start the writing
+//		try {
+//			// Create file 
+//			  FileWriter fstream = new FileWriter(tag+".txt");
+//			  BufferedWriter outFile = new BufferedWriter(fstream);
+//			 
+//			  outFile.write("<table border=1>\n");
+//			 outFile.write("<tr><td><h3>Lecturer</h3><h4>"+ lectureName + "</h4></td><td><h3>Room</h3><h4>#"+ tag + "</h4></td></tr>\n");
+//			 for (int i=0; i < ltArray.length;i++){
+//				 outFile.write("<tr>\n");
+//				 outFile.write("<td valign='top' width='55%'>" +URLInString.makeURLInString(ltArray[i].getText()) + "</td><td bgcolor='#E0E0E0' valign='top'> <td>\n</tr>\n");
+//				 //F8F8F8
+//				 //outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
+//				 if (i < ltArray.length -1){
+//					 for (int j =0; j < ttArray.length; j++){
+//						 if (ttArray[j].getCreatedAt().after(ltArray[i].getCreatedAt()) 
+//								 && ttArray[j].getCreatedAt().before(ltArray[i+1].getCreatedAt())){
+//							 outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
+//							 outFile.write("<a href='https://twitter.com/#!/"
+//									 + ttArray[j].getFromUser() 
+//									 +  "' target='_blank'>@" +ttArray[j].getFromUser() + "</a>\n");
+//							 outFile.write(URLInString.makeURLInString(ttArray[j].getText()) + "</td></tr>\n"); 
+//						 }	 
+//					 }
+//				 }
+//				 else {
+//					 for (int j =0; j < ttArray.length; j++){
+//						 if (ttArray[j].getCreatedAt().after(ltArray[i].getCreatedAt())){
+//							 outFile.write("<tr><td> </td><td bgcolor='#E0E0E0' valign='top'>");
+//							 outFile.write("<a href='https://twitter.com/#!/"
+//									 + ttArray[j].getFromUser() 
+//									 +  "' target='_blank'>@" +ttArray[j].getFromUser() + "</a>\n");
+//							 outFile.write(URLInString.makeURLInString(ttArray[j].getText()) + "</td></tr>\n"); 
+//						 }	 
+//					 }
+//					 
+//				 }
+//					 
+//				 //outFile.write("</td>");
+//				 //outFile.write("</tr>\n");
+//				 writeProgressBar.setValue(i);
+//				 writeProgressBar.repaint();
+//				
+//				 
+//				 
+//			 }
+//			 outFile.write("</table>\n");
+//			//Close the output stream
+//			 outFile.close();
+//			 //System.out.println("Done");
+//			 writeProgressBar.setIndeterminate(false);
+//			 
+//		} catch (IOException e) {
+//			System.err.println("Error: " + e.getMessage());
+//		}
+//	}
 	
 	private void getAllTextFields(){
 		tag = removeChar(tagTextField.getText(), '#');
